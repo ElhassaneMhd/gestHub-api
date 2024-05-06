ee0c1059ee5468e471e1b85a67a5c57f6dc41d11
Elhassane MEHDIOUI <121520500+ElhassaneMhd@users.noreply.github.com>
2024-05-06 16:21:52 +0100

    comoser no dev

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev 
 
 echo "generate key..."
 php artisan key:generate
