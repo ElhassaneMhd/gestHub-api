b80631ae5d5afc11bf8fb0f32add9312ecbd66ec
Winnery05 <elmedhassan2002@gmail.com>
2024-05-11 00:51:09 +0100

    deploye 1

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global update
+composer global update --working-dir=/var/www/html
 composer install --no-dev  --working-dir=/var/www/html
 
 echo "Caching config..."
