2eab65d8881ce25d4d7ae55af1ba9903356c2d38
Winnery05 <elmedhassan2002@gmail.com>
2024-05-18 00:41:49 +0100

    remove faker

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -29,54 +29,54 @@ class ProfileSeeder extends Seeder
         ]);
 
 
-$faker = Faker::create();
+// $faker = Faker::create();
 
-$roles = ['user','intern', 'supervisor', 'admin', 'super-admin'];
+// $roles = ['user','intern', 'supervisor', 'admin', 'super-admin'];
 
-foreach ($roles as $role) {
-    for ($i = 0; $i < 5; $i++) {
-        $profile = new Profile;
-        $profile->firstName = $faker->firstName;
-        $profile->lastName = $faker->lastName;
-        $profile->email = $profile->firstName.'@gmail.com';
-        $profile->password = Hash::make($profile->firstName.'123');
-        $profile->phone = $faker->phoneNumber;
-        $profile->gender = $faker->randomElement(['M', "Mme"]);
-        $profile->assignRole($role);
-        $profile->save();
+// foreach ($roles as $role) {
+//     for ($i = 0; $i < 5; $i++) {
+//         $profile = new Profile;
+//         $profile->firstName = $faker->firstName;
+//         $profile->lastName = $faker->lastName;
+//         $profile->email = $profile->firstName.'@gmail.com';
+//         $profile->password = Hash::make($profile->firstName.'123');
+//         $profile->phone = $faker->phoneNumber;
+//         $profile->gender = $faker->randomElement(['M', "Mme"]);
+//         $profile->assignRole($role);
+//         $profile->save();
 
-        switch ($role) {
-            case 'user':
-                DB::table('users')->insert([
-                    'profile_id' => $profile->id,
-                    'academicLevel' => 'Bac+2',
-                    'establishment' => 'Ofppt',
-                ]);
-                break;
-            case 'supervisor':
-                DB::table('supervisors')->insert([
-                    'profile_id' => $profile->id,
-                ]);
-                break;
-            case 'admin':
-            case 'super-admin':
-                DB::table('admins')->insert([
-                    'profile_id' => $profile->id,
-                ]);
-                break;
-            case 'intern':
-                DB::table('interns')->insert([
-                    'profile_id' => $profile->id,
-                    'academicLevel' => 'Bac+2',
-                    'establishment' => 'Ofppt',
-                    'specialty' => 'Développement',
-                    'startDate' => $faker->dateTimeBetween('-1 years', '+1 years')->format('Y-m-d H:i:s'),
-                    'endDate' => $faker->dateTimeBetween('+1 years', '+2 years')->format('Y-m-d H:i:s'),
-                ]);
-                break;
-        }
-    }
-    }
+//         switch ($role) {
+//             case 'user':
+//                 DB::table('users')->insert([
+//                     'profile_id' => $profile->id,
+//                     'academicLevel' => 'Bac+2',
+//                     'establishment' => 'Ofppt',
+//                 ]);
+//                 break;
+//             case 'supervisor':
+//                 DB::table('supervisors')->insert([
+//                     'profile_id' => $profile->id,
+//                 ]);
+//                 break;
+//             case 'admin':
+//             case 'super-admin':
+//                 DB::table('admins')->insert([
+//                     'profile_id' => $profile->id,
+//                 ]);
+//                 break;
+//             case 'intern':
+//                 DB::table('interns')->insert([
+//                     'profile_id' => $profile->id,
+//                     'academicLevel' => 'Bac+2',
+//                     'establishment' => 'Ofppt',
+//                     'specialty' => 'Développement',
+//                     'startDate' => $faker->dateTimeBetween('-1 years', '+1 years')->format('Y-m-d H:i:s'),
+//                     'endDate' => $faker->dateTimeBetween('+1 years', '+2 years')->format('Y-m-d H:i:s'),
+//                 ]);
+//                 break;
+//         }
+//     }
+//     }
 
          
     }
