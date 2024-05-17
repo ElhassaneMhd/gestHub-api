7fc8a3ce105a2ecaa89e37d5b604d7c7ef0dd5f5
Winnery05 <elmedhassan2002@gmail.com>
2024-05-17 23:51:35 +0100

    deploy-laravel

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,8 +2,8 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -12,4 +12,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
