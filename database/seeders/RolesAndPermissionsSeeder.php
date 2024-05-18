eec96fa216cef1759bd79e24a5861d68893a3375
Winnery05 <elmedhassan2002@gmail.com>
2024-05-18 14:29:30 +0100

    remove api guards

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -13,21 +13,15 @@ class RolesAndPermissionsSeeder extends Seeder
         // Reset cached roles and permissions
     
         Permission::create(['name' => 'store admins']);
-        Permission::create(['guard_name'=>'api','name' => 'store admins']);
 
         Role::create(['name' => 'supervisor']);
-        Role::create(['guard_name'=>'api', 'name' => 'supervisor']);
 
         Role::create(['name' => 'admin']);
-        Role::create(['guard_name'=>'api','name' => 'admin']);
         
         Role::create(['name' => 'intern']);
-        Role::create(['guard_name'=>'api','name' => 'intern']);
 
         Role::create(['name' => 'user']);
-        Role::create(['guard_name'=>'api','name' => 'user']);
         
         Role::create(['name' => 'super-admin'])->givePermissionTo('store admins');
-        Role::create(['guard_name'=>'api','name' => 'super-admin'])->givePermissionTo('store admins');
     }
 }
