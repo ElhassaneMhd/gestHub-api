dc669dc5d19d8601d7808dae0804ab7b9d0fd200
Winnery05 <elmedhassan2002@gmail.com>
2024-05-23 11:57:06 +0100

    edit updat taskObserser

--- app/Observers/taskObserser.php
+++ app/Observers/taskObserser.php
@@ -15,7 +15,7 @@ class taskObserser
     }
 
     public function updated(Task $task): void{
-        $data = ['action' => 'Update', 'model' => 'Task', 'activity'=>'Change task status to '. $task->status.' : ','object'=>$task->project->subject . '/' . $task->title ];
+        $data = ['action' => 'Update', 'model' => 'Task', 'activity'=>'Change task status to : '. $task->status,'object'=>$task->project->subject . '/' . $task->title ];
         $this->storeActivite($data);
     }
 
