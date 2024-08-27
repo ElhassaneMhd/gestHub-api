c7ecce52b5edb17dd7984ed0399129fa1c55ef25
Winnery05 <elmedhassan2002@gmail.com>
2024-08-27 16:57:40 +0100

    test mailing

--- resources/views/mails/welcome.blade.php
+++ resources/views/mails/welcome.blade.php
@@ -3,22 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Attestation de Stage</title>
+  <title>Mail</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" >
-  <style>
-    .contImage{
-      padding: 0 0 0 60px;
-    }
-    .footer{
-
-      font-size:15px;
-      padding-top:300px;
-    }
-  </style>
 </head>
 <body class=' col justify-between'>
-  <h2>{{$subject}}</h2>
-  <p>{{$message}}</p>
+  {{-- <h2>{{$subject}}</h2>
+  <p>{{$message}}</p> --}}
+  <h1>Welcome everybody</h1>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
