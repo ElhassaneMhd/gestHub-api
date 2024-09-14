2d49ec58b0260251c6f5b2353d78e24a7507e8c9
Winnery05 <elmedhassan2002@gmail.com>
2024-09-14 17:46:56 +0100

    add laravel sanctum support

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,https://gesthub.netlify.app',
+        'localhost,localhost:5137,127.0.0.1,127.0.0.1:8000,::1,https://gesthub.netlify.app',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
