40cda081ab52a2f58706ee4e5eaa303f56e8337f
Winnery05 <elmedhassan2002@gmail.com>
2024-09-14 16:35:53 +0100

    test new frontEnd origin

--- app/Http/Middleware/CheckOrigin.php
+++ app/Http/Middleware/CheckOrigin.php
@@ -14,19 +14,19 @@ class CheckOrigin
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response{
-        $allowedOrigins = 'https://gesthub.netlify.app'; 
+        $allowedOrigins = 'http://localhost:5173';
         $origin = $request->headers->get('Origin');
         $acceptPath = $request->headers->get('Accept-Path');
         if (env('APP_ENV')==='devloppement'){
-            return $next($request);     
+            return $next($request);
         }
         if($origin){
             if (!$acceptPath || !$origin===$allowedOrigins) {
                 return response()->json(['message'=>'Unauthorized to perform this action' ], 401);
             }
-            return $next($request);     
+            return $next($request);
         }
-       
+
         return $next($request);
     }
 }
