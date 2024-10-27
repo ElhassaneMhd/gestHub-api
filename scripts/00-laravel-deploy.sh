b0e642738039ae425b273329c21dc8c8feaaed94
Winnery05 <elmedhassan2002@gmail.com>
2024-10-27 21:56:05 +0100

    remove seeding

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,8 +2,8 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -12,7 +12,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --seed --force
+php artisan migrate --force
 
 echo "Optimize"
 php artisan optimize
