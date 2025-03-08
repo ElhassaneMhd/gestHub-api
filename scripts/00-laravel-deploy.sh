eff2977826dca3e65e24a665e4adbc63a123f494
Winnery05 <elmedhassan2002@gmail.com>
2025-03-08 15:09:16 +0000

    remove unique constraint from 2019_08_19_000000_create_failed_jobs_table

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,17 +5,14 @@ composer install --no-dev --working-dir=/var/www/html
 # echo "generating application key..."
 # php artisan key:generate --show
 
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
 
 php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
+echo "Optimize"
+php artisan optimize
 
 echo "Running migrations..."
 # php artisan migrate --force
@@ -23,8 +20,7 @@ echo "Running migrations..."
 php artisan migrate:reset
 php artisan migrate:fresh --seed --force
 
-echo "Optimize"
-php artisan optimize
+
 
 mkdir storage/framework/{cache/data,views}
 mkdir public/attestation
