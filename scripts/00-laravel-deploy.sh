55426768800f6a39149c76d8f1148a0a7bbb6181
Winnery05 <elmedhassan2002@gmail.com>
2025-03-08 17:51:24 +0000

    remove clear

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,21 +4,15 @@ composer install --no-dev --working-dir=/var/www/html
 
 # echo "generating application key..."
 # php artisan key:generate --show
-
-
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
+# php artisan config:clear
+# php artisan cache:clear
+# php artisan route:clear
+# php artisan view:clear
 
 echo "Optimize"
 php artisan optimize
 
 echo "Running migrations..."
-# php artisan migrate --force
-# php artisan migrate --seed --force
-# php artisan migrate:reset
-# php artisan migrate:fresh --seed --force
 php artisan db:seed
 
 
